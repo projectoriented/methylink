7fca55a1110f1d21156480fc06e13f8acfd8693d
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2022-06-24 14:55:58 -0700

    write out bed

--- workflow/scripts/prioritize_pav_calls.py
+++ workflow/scripts/prioritize_pav_calls.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 
 import pybedtools
-
-PAV_HEADER = ["#CHROM", "POS", "END", "ID", "SVTYPE", "SVLEN", "HAP", "GT", "CLUSTER_MATCH", "CALL_SOURCE",
-              "TIG_REGION", "QUERY_STRAND", "ALIGN_INDEX", "LEFT_SHIFT", "HOM_REF", "HOM_TIG", "HAP_VARIANTS", "CI",
-              "HAP_RO", "HAP_OFFSET", "HAP_SZRO", "HAP_OFFSZ"]
+import pandas as pd
 
 
-# filter out low-complexity regions
-def remove_low_complexity(insertion_bed, lc_bed):
-    a = pybedtools.BedTool(insertion_bed)
-    b = pybedtools.BedTool(lc_bed)
-    return a.intersect(b, v=True)
-
-
-# filter for coding regions
-def get_cds(insertion_bed, gencode_bed):
-    a = pybedtools.BedTool(insertion_bed)
-    b = pybedtools.BedTool(gencode_bed)
-    return a.intersect(b, u=True)
-
-
-def get_sizes(filtered_bed_obj, size=10000):
-    df = filtered_bed_obj.to_dataframe(header=None, keep_default_na=False, names=PAV_HEADER)
+def filter_sizes(bed_file, size=10000):
+    df = pd.read_csv(bed_file, header=True, keep_default_na=False)
     return df.loc[(df['SVLEN'] >= size)]
+
+
+def filter_bed(query_bed, lc_bed, gencode_bed, out_file):
+    query = pybedtools.BedTool(query_bed)
+    lc = pybedtools.BedTool(lc_bed)
+    gencode = pybedtools.BedTool(gencode_bed)
+
+    # remove low complexity regions
+    lc_removed = query.intersect(lc, v=True)
+    # filter for coding regions - this is not strand aware
+    return lc_removed.intersect(gencode, u=True, output=out_file)
+
+
+filter_bed(filter_sizes(snakemake.input.pav_ins_bed), snakemake.input.lc_bed, snakemake.input.gencode_bed, snakemake.output.out_file)
+
