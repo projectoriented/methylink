f931ce872c5f6c8438c38c420032e651d1c51db8
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-04-19 21:41:14 -0700

    going for db instead of pickle bc meth dict is TOO LARGE

--- workflow/scripts/append_mod_tags.py
+++ workflow/scripts/append_mod_tags.py
@@ -5,9 +5,10 @@ import sys
 import time
 import math
 import itertools
-import gzip
 import pickle
+
 from multiprocessing import Pool
+import sqlite3
 
 import pysam
 
@@ -15,44 +16,76 @@ import pysam
 sys.stdout = open(snakemake.log[0], "w")
 
 
+def create_database(db_name):
+    if os.path.exists(db_name):
+        os.remove(db_name)
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('CREATE TABLE meth_tags (qname TEXT PRIMARY KEY, tag BLOB)')
+
+    # commit changes and close connection
+    conn.commit()
+    conn.close()
+
+
 def get_time():
     time_rn = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     return time_rn
 
 
-def fetch_modified_bases(modified_obj) -> dict:
+def fetch_modified_bases(modified_obj, db_name):
     """
     Fetch base modification tags Mm & Ml
     :param modified_obj: An unsorted bam pysam object with just methylation calls
+    :param db_name:
     :return: A dictionary of tags where keys = query name and value = list of optional tags
     """
-    tags_dict = {}
     print(f"Opening {modified_obj.filename.decode()} to fetch tags. {get_time()}")
+
+    # Start database connection
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
 
     for read in modified_obj.fetch(until_eof=True):
         if read.has_tag("Mm") or read.has_tag("MM"):
             tags = read.get_tags()
             qname = read.query_name
-            tags_dict[qname] = tags
+
+            # serialize the tags list
+            serialized_list = pickle.dumps(tags)
+            c.execute("INSERT INTO meth_tags VALUES (?, ?)", (str(qname), sqlite3.Binary(serialized_list)))
+
     modified_obj.close()
 
+    # commit changes and close connection
+    conn.commit()
+    conn.close()
+
     print(f"Base modification tags fetched for {modified_obj.filename.decode()}. {get_time()}")
-    return tags_dict
 
 
-def write_linked_tags(bam, tags_dict, out_file) -> None:
+def write_linked_tags(bam, db_name, out_file) -> None:
     """
     Write out merged bam with Mm tags and possibly Ml, and its index.
     :param bam: equivalent aligned bam
-    :param tags_dict: a dict of {query_name: [Mm tags and possibly Ml]}
+    :param db_name: a dict of {query_name: [Mm tags and possibly Ml]}
     :param out_file: merged bam file path
     :return: None
     """
+
+    # Connect to db
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
     appended_tags = pysam.AlignmentFile(out_file, "wb", template=bam)
     for read in bam.fetch(until_eof=True):
-        if read.query_name in tags_dict.keys():
-            read.set_tags(read.get_tags() + tags_dict[read.query_name])
+        result = c.execute("SELECT tag FROM meth_tags WHERE qname = ?", (str(read.qname),)).fetchone()
+        if result:
+            deserialized_tag = pickle.loads(result[0])
+            read.set_tags(read.get_tags() + deserialized_tag)
         appended_tags.write(read)
+
     print(f"File written to: {out_file}")
     appended_tags.close()
 
@@ -61,24 +94,23 @@ def write_linked_tags(bam, tags_dict, out_file) -> None:
     print(f"Index written for {out_file}.bai")
 
 
-def collect_tags(methyl_sn_input: list) -> dict:
+def collect_tags(methyl_sn_input: list, db_name):
     # methyl_sn_input: snakemake input
     """
     Collect optional tags from ONT bam with methyl calls
     :param methyl_sn_input: a list of file paths pointing to methyl bam
+    :param db_name:
     :return: a dict of {query_name: [Mm tags and possibly Ml]}
     """
-    tags = {}
+    # tags = {}
     if not len(methyl_sn_input) == 1:
         for bam in methyl_sn_input:
             methyl_bam = pysam.AlignmentFile(bam, "rb", check_sq=False)
-            dict_of_tags_per_bam = fetch_modified_bases(methyl_bam)
-            tags.update(dict_of_tags_per_bam)
+            fetch_modified_bases(methyl_bam, db_name)
+            # tags.update(dict_of_tags_per_bam)
     else:
         methyl_bam = pysam.AlignmentFile(methyl_sn_input[0], "rb", check_sq=False)
-        dict_of_tags_per_bam = fetch_modified_bases(methyl_bam)
-        tags.update(dict_of_tags_per_bam)
-    return tags
+        fetch_modified_bases(methyl_bam, db_name)
 
 
 def make_subset_bams(bam, n_splits, prefix):
@@ -117,13 +149,10 @@ def combine_the_chunked(bams: list[str], merge_output: str):
     os.remove(f'{merge_output}.unsorted')
 
 
-def run_pool(bam_file: str, tags_dict_pkl: str, output_file) -> None:
+def run_pool(bam_file: str, db_name, output_file) -> None:
     aln_bam = pysam.AlignmentFile(bam_file, "rb")
 
-    with gzip.open(tags_dict_pkl, 'rb') as f:
-        tags_dict = pickle.load(f)
-
-    write_linked_tags(aln_bam, tags_dict, output_file)
+    write_linked_tags(aln_bam, db_name, output_file)
 
     wait_time = 1  # wait 1 second between each check
 
@@ -132,7 +161,7 @@ def run_pool(bam_file: str, tags_dict_pkl: str, output_file) -> None:
         time.sleep(wait_time)
 
     # file is available, remove it
-    os.remove(bam_file)
+    clean_up_temps([bam_file])
 
 
 def clean_up_temps(files: list):
@@ -156,6 +185,9 @@ def main():
     prefix = os.path.join(snakemake.resources.tmpdir, snakemake.wildcards.sample)
     final_output = snakemake.output.linked_bam
 
+    db_name=f'{prefix}-my_db.db'
+    create_database(db_name=db_name)
+
     # Make sure that each chunk is roughly 100 MB
     file_size = os.path.getsize(aln_bam)
     chunk_size = 100 * 1024 * 1024  # 100 MB in bytes
@@ -167,11 +199,7 @@ def main():
     n = 10 if file_size < chunk_size else get_n_splits()
 
     # Get the meth dictionary
-    tags_dict = collect_tags(methyl_collection)
-    pkl_out = f'{prefix}_tags-dict.pickle.gz'
-    with gzip.open(pkl_out, 'wb') as f:
-        pickle.dump(tags_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
-    del tags_dict
+    collect_tags(methyl_collection, db_name=db_name)
 
     # Make the chunks
     make_subset_bams(bam=bam, n_splits=n, prefix=prefix)
@@ -181,16 +209,15 @@ def main():
     link_bam_output_names = [f"{prefix}_tmp.{idx}-linked.bam" for idx in range(0, n)]
 
     with Pool(threads) as p:
-        p.starmap(run_pool, zip(chunked_bams, itertools.repeat(pkl_out), link_bam_output_names))
+        p.starmap(run_pool, zip(chunked_bams, itertools.repeat(db_name), link_bam_output_names))
         p.close()
         p.join()
 
     combine_the_chunked(link_bam_output_names, final_output)
 
     # CLEANING UP!
-    clean_up_temps(chunked_bams)
     clean_up_temps(link_bam_output_names)
-    clean_up_temps([pkl_out])
+    clean_up_temps([db_name])
 
 
 if __name__ == '__main__':
