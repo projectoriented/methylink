67240c53493753774171cd2d0143b7585fb50850
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-05-22 07:59:35 -0700

    include readme

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
+import io
+import os
+
 from setuptools import setup, find_packages
 
 
+AUTHOR = "Mei Wu"
+DESCRIPTION = "methylink is a tool to link methylation tags between SAM/BAM files."
+HERE = os.path.abspath(os.path.dirname(__file__))
+NAME = "methylink"
 REQUIREMENTS = ["click >= 8.1.3", "pysam >= 0.21.0", "pytest"]
 
+try:
+    with io.open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
+        LONG_DESCRIPTION = "\n" + f.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
 setup(
-    name="methylink",
+    name=NAME,
     version="0.1.0",
-    author="Mei Wu",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
     packages=find_packages(),
     include_package_data=True,
+    url="https://github.com/projectoriented/methylink",
     install_requires=[REQUIREMENTS],
     python_requires=">=3.9.13",
     entry_points={
@@ -16,4 +32,9 @@ setup(
             "methylink = methylink.main:base",
         ],
     },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Environment :: Console",
+    ],
 )
